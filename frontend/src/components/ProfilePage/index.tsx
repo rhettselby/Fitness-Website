8eb32b5acbb4aeb07400a01b5b0e7e3619762980
Rhett Selby <rhettselby10@gmail.com>
2025-12-04 14:44:43 -0800

    fix case-sensitive files configurations

--- frontend/src/components/ProfilePage/index.tsx
+++ frontend/src/components/ProfilePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "@/components/navbar";
 import { SelectedPage } from "@/shared/types";
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useNavigate, useLocation } from "react-router-dom";
 import UCLA_Logo from "@/assets/UCLA_Logo.svg";
