4fe2356427279c8422af9e8a8cf475cc16b8bdad
Rhett Selby <rhettselby10@gmail.com>
2025-12-26 12:00:42 -0800

    debug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App'
 
+console.log("BUILD ID: ", Date.now());
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
