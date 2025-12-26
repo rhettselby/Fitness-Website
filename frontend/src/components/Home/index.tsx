856bf4eebae5fd8947ecd1675d6f4b8ed843564c
Rhett Selby <rhettselby10@gmail.com>
2025-12-26 10:57:02 -0800

    removed sponsors

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -101,42 +101,6 @@ const Home = ({ setSelectedPage }: Props) => {
         </div>
       </motion.div>
 
-      {/* Sponsors */}
-      {isAboveMediumScreen && (
-        <div className="h-[150px] w-full bg-primary-100 py-10">
-          <div className="mx-auto w-5/6">
-            <div className="flex w-3/5 items-center justify-between gap-8">
-              <img
-                alt="redbull-sponsor"
-                src={SponsorRedBull}
-                className="h-12 w-auto object-contain"
-                onError={(e) => {
-                    console.error("Failed to load SponsorRedBull:", SponsorRedBull);
-                    e.currentTarget.style.display = 'none';
-                }}
-              />
-              <img
-                alt="forbes-sponsor"
-                src={SponsorForbes}
-                className="h-12 w-auto object-contain"
-                onError={(e) => {
-                    console.error("Failed to load SponsorForbes:", SponsorForbes);
-                    e.currentTarget.style.display = 'none';
-                }}
-              />
-              <img
-                alt="fortune-sponsor"
-                src={SponsorFortune}
-                className="h-12 w-auto object-contain"
-                onError={(e) => {
-                    console.error("Failed to load SponsorFortune:", SponsorFortune);
-                    e.currentTarget.style.display = 'none';
-                }}
-              />
-            </div>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
