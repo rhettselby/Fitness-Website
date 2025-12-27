53896ec7f76314e6d31d1df75be016e71cd40d37
Rhett Selby <rhettselby10@gmail.com>
2025-12-27 12:17:26 -0800

    befentis links

--- frontend/src/components/Benefits/Benefit.tsx
+++ frontend/src/components/Benefits/Benefit.tsx
@@ -39,7 +39,7 @@ const Benefit = ({icon, title, description, setSelectedPage, linkTo}: Props) => 
     return (
         <motion.div 
         variants={childVariant}
-        className = "mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center flex-1 max-w-sm min-h-[350px] "> 
+        className = "mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center flex-1 max-w-sm min-h-[350px]"> 
             <div className = "mb-4 flex justify-center">
                 <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
                     {icon}
@@ -47,15 +47,23 @@ const Benefit = ({icon, title, description, setSelectedPage, linkTo}: Props) => 
             </div>
             <h4 className="font-bold"> {title}</h4>
             <p className="my-3"> {description} </p>
-            <AnchorLink
-                className="text-sm font-bold text-primary-500 underline"
-                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-                href={`#${SelectedPage.ContactUs}`}
-            >
-            <p>Learn More</p>
-            </AnchorLink>
-        
-        
+            
+            {isRoute ? (
+                <button
+                    className="text-sm font-bold text-primary-500 underline hover:text-primary-300 cursor-pointer"
+                    onClick={handleClick}
+                >
+                    <p>Learn More</p>
+                </button>
+            ) : (
+                <AnchorLink
+                    className="text-sm font-bold text-primary-500 underline hover:text-primary-300"
+                    onClick={handleClick}
+                    href={`#${linkTo || SelectedPage.ContactUs}`}
+                >
+                    <p>Learn More</p>
+                </AnchorLink>
+            )}
         </motion.div>
     )
 }
