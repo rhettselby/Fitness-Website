777f208c56740a9155e9d1f47b99ca0a4889ff71
Rhett Selby <rhettselby10@gmail.com>
2025-12-26 21:46:40 -0800

    changed action button

--- frontend/src/shared/ActionButton.tsx
+++ frontend/src/shared/ActionButton.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import { SelectedPage } from "./types";
 
-
 type Props = {
     children: React.ReactNode;
     setSelectedPage:(value:SelectedPage)=>void
@@ -11,11 +10,10 @@ type Props = {
 const ActionButton = ({ children, setSelectedPage }: Props) => {
     return (
         <AnchorLink
-            className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+            className="rounded-md bg-accent-500 text-white px-10 py-2 hover:bg-accent-600 font-bold transition duration-300"
             onClick={() => setSelectedPage(SelectedPage.ContactUs)}
             href = {`#${SelectedPage.ContactUs}`}
         >
-            
             {children}
         </AnchorLink>
     )
