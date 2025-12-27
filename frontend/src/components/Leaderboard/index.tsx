19a5ba32ff53090fe54da5e005e9031c8b7795e9
Rhett Selby <rhettselby10@gmail.com>
2025-12-27 15:01:33 -0800

    widen leaderboard

--- frontend/src/components/Leaderboard/index.tsx
+++ frontend/src/components/Leaderboard/index.tsx
@@ -58,7 +58,7 @@ const Leaderboard = ({ setSelectedPage }: Props) => {
 
   return (
     <section id="leaderboard" className="w-full bg-primary-100 py-20">
-      <div className="max-w-4xl mx-auto px-4">
+      <div className="max-w-7xl mx-auto px-4">
         <motion.div
           onViewportEnter={() => setSelectedPage(SelectedPage.Leaderboard)}
         >
