74d640e2a497de354dc4a342f4e3f538e84c77b6
Rhett Selby <rhettselby10@gmail.com>
2025-12-26 22:04:36 -0800

    colors

--- frontend/src/components/Benefits/Benefit.tsx
+++ frontend/src/components/Benefits/Benefit.tsx
@@ -3,14 +3,12 @@ import type { JSX } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import {motion} from "framer-motion"
 
-
 type Props = {
     icon: React.ReactNode;
     title: string;
     description: string;
     setSelectedPage: (value:SelectedPage) => void;
 }
-
 
 const childVariant = {
     hidden: {opacity: 0, scale: 0.9 },
@@ -21,16 +19,16 @@ const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
     return (
         <motion.div 
         variants={childVariant}
-        className = "mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"> 
+        className = "mt-5 rounded-md border-2 border-gray-100 bg-gray-50 px-5 py-16 text-center"> 
             <div className = "mb-4 flex justify-center">
-                <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
+                <div className="rounded-full border-2 border-primary-500 bg-primary-500 p-4">
                     {icon}
                 </div>
             </div>
-            <h4 className="font-bold"> {title}</h4>
-            <p className="my-3"> {description} </p>
+            <h4 className="font-bold text-white"> {title}</h4>
+            <p className="my-3 text-gray-100"> {description} </p>
             <AnchorLink
-                className="text-sm font-bold text-primary-500 underline"
+                className="text-sm font-bold text-accent-500 underline hover:text-accent-400"
                 onClick={() => setSelectedPage(SelectedPage.ContactUs)}
                 href={`#${SelectedPage.ContactUs}`}
             >
